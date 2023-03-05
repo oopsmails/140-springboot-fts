a71c1ef81c4cc3b1778777836f133c0fa8ee8eea
oopsmails <oopsmails@gmail.com>
2023-03-04 21:42:00 -0500

    20230304: Lucene testing

--- springboot-lucene/src/main/java/com/oopsmails/lucenesearch/service/TradeItemSearchService.java
+++ springboot-lucene/src/main/java/com/oopsmails/lucenesearch/service/TradeItemSearchService.java
@@ -47,19 +47,7 @@ public class TradeItemSearchService {
 
         String symbolTxt = param.getSymbolTxt();
         if (symbolTxt != null && !"".equals(symbolTxt)) {
-            if (symbolTxt.length() <= 2) {
-                Query query = new TermQuery(new Term(TradeItemIndexer.FIELD_SYMBOL, symbolTxt.toUpperCase()));
-                queryBuilder.add(query, BooleanClause.Occur.MUST);
-            } else {
-                Query querySymbol = new PrefixQuery(new Term(TradeItemIndexer.FIELD_SYMBOL, symbolTxt.toUpperCase()));
-                queryBuilder.add(querySymbol, BooleanClause.Occur.MUST);
-
-                Query queryName = new WildcardQuery(new Term(TradeItemIndexer.FIELD_NAME, "*" + symbolTxt + "*"));
-                queryBuilder.add(queryName, BooleanClause.Occur.SHOULD);
-
-                Query queryDesc = new WildcardQuery(new Term(TradeItemIndexer.FIELD_DESC, "*" + symbolTxt + "*"));
-                queryBuilder.add(queryDesc, BooleanClause.Occur.SHOULD);
-            }
+            addSymbolQuery(queryBuilder, param);
         }
 
         BooleanQuery booleanQuery = queryBuilder.build();
@@ -71,5 +59,74 @@ public class TradeItemSearchService {
         return result;
     }
 
-//    private Query get
+    private BooleanQuery.Builder addSymbolQuery(BooleanQuery.Builder queryBuilder, TradeItemSearchParam param) {
+        String typeTxt = param.getTypeTxt();
+        String symbolTxt = param.getSymbolTxt();
+        if (typeTxt != null && !"".equals(typeTxt)) {
+            switch (typeTxt.trim()) {
+                case "STOCK":
+                case "FIXEDINCOME":
+                    addSymbolQuery(queryBuilder, param, "Term");
+                    break;
+                case "FUND":
+                    addSymbolQuery(queryBuilder, param, "Prefix");
+                    break;
+                case "OPTION":
+                    addSymbolQuery(queryBuilder, param, "Wildcard");
+                    break;
+                default:
+                    System.out.println("Invalid day of the week.");
+                    break;
+            }
+        } else {
+            if (symbolTxt != null && !"".equals(symbolTxt)) {
+                if (symbolTxt.length() <= 2) {
+                    // need to work on different types and merge!!!
+                    addSymbolQuery(queryBuilder, param, "Term");
+                } else {
+                    addSymbolQuery(queryBuilder, param, "Prefix");
+                    addNameQuery(queryBuilder, param);
+                    addDescQuery(queryBuilder, param);
+                }
+            } else {
+                log.warn("Should not be here ....");
+                return queryBuilder;
+            }
+        }
+
+        return queryBuilder;
+    }
+
+    private BooleanQuery.Builder addSymbolQuery(BooleanQuery.Builder queryBuilder, TradeItemSearchParam param, String queryType) {
+        String symbolTxt = param.getSymbolTxt().trim().toUpperCase();
+        if ("Wildcard".equals(queryType)) {
+            Query querySymbol = new WildcardQuery(new Term(TradeItemIndexer.FIELD_SYMBOL, "*" + symbolTxt + "*"));
+            queryBuilder.add(querySymbol, BooleanClause.Occur.MUST);
+        } else if ("Prefix".equals(queryType)) {
+            Query querySymbol = new PrefixQuery(new Term(TradeItemIndexer.FIELD_SYMBOL, symbolTxt));
+            queryBuilder.add(querySymbol, BooleanClause.Occur.MUST);
+        } else {
+            Query querySymbol = new TermQuery(new Term(TradeItemIndexer.FIELD_SYMBOL, symbolTxt));
+            queryBuilder.add(querySymbol, BooleanClause.Occur.MUST);
+        }
+
+        return queryBuilder;
+    }
+
+
+    private BooleanQuery.Builder addNameQuery(BooleanQuery.Builder queryBuilder, TradeItemSearchParam param) {
+        String symbolTxt = param.getSymbolTxt();
+        Query queryName = new WildcardQuery(new Term(TradeItemIndexer.FIELD_NAME, "*" + symbolTxt + "*"));
+        queryBuilder.add(queryName, BooleanClause.Occur.SHOULD);
+
+        return queryBuilder;
+    }
+
+    private BooleanQuery.Builder addDescQuery(BooleanQuery.Builder queryBuilder, TradeItemSearchParam param) {
+        String symbolTxt = param.getSymbolTxt();
+        Query queryDesc = new WildcardQuery(new Term(TradeItemIndexer.FIELD_DESC, "*" + symbolTxt + "*"));
+        queryBuilder.add(queryDesc, BooleanClause.Occur.SHOULD);
+
+        return queryBuilder;
+    }
 }
